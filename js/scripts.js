046cfa5ab35bd24c901091bedb44bc3ae76fc203
Emilie Thoreson and Witty Chang <witty@>
2017-05-31 09:45:38 -0700

    show def/ hide word & hide word / show def when toggle

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+
+  $("#js").click(function() {
+    $("#jsdef").toggle();
+    $("#js").toggle();
+  });
+
+  $("#jsdef").click(function() {
+    $("#js").toggle();
+    $("#jsdef").toggle();
+  });
+
+  $("#op").click(function() {
+    $("#opdef").toggle();
+    $("#op").toggle();
+  });
+
+  $("#opdef").click(function() {
+    $("#op").toggle();
+    $("#opdef").toggle();
+  });
+
+  $("#variable").click(function() {
+    $("#variabledef").toggle();
+    $("#variable").toggle();
+  });
+
+  $("#variabledef").click(function() {
+    $("#variable").toggle();
+    $("#variabledef").toggle();
+  });
+
+  $("#convention").click(function() {
+    $("#conventiondef").toggle();
+    $("#convention").toggle();
+  });
+
+  $("#conventiondef").click(function() {
+    $("#convention").toggle();
+    $("#conventiondef").toggle();
+  });
+
+  $("#function").click(function() {
+    $("#functiondef").toggle();
+    $("#function").toggle();
+  });
+
+  $("#functiondef").click(function() {
+    $("#function").toggle();
+    $("#functiondef").toggle();
+  });
+
+  $("#method").click(function() {
+    $("#methoddef").toggle();
+    $("#method").toggle();
+  });
+
+  $("#methoddef").click(function() {
+    $("#method").toggle();
+    $("#methoddef").toggle();
+  });
+
+  $("#argspar").click(function() {
+    $("#argspardef").toggle();
+    $("#argspar").toggle();
+  });
+
+  $("#argspardef").click(function() {
+    $("#argspar").toggle();
+    $("#argspardef").toggle();
+  });
+
+  $("#return").click(function() {
+    $("#returndef").toggle();
+    $("#return").toggle();
+  });
+
+  $("#returndef").click(function() {
+    $("#return").toggle();
+    $("#returndef").toggle();
+  });
+
+
+
+
+})
